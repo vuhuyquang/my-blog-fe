53554d1c2ea947947c646a9591c30c8b662f535b
unknown <vuhuyquang2k@gmail.com>
2024-04-19 00:16:31 +0700

    update code

---
+++ nuxt.config.ts
@@ -0,0 +1,8 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@ant-design-vue/nuxt',
+    '@nuxtjs/tailwindcss',
+  ],
+  devtools: { enabled: false }
+})
---
+++ pages/product/[id].vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    Product page {{ id }}
+  </div>
+</template>
+
+<script setup>
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+
+const id = route.params.id;
+</script>
