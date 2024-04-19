fc95b0182189ce94a376a1853686de0e5853b9a3
quangvh18 <quangvh18@ghtk.co>
2024-04-19 19:22:47 +0700

    update code

---
+++ components/Header/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <header class="drop-shadow-xl bg-white text-[14px] text-[black]">
+    <nav
+      class="container mx-auto px-5 h-[76px] flex justify-between items-center"
+    >
+      <div class="grid grid-flow-col auto-cols-max gap-2">
+        <img
+          class="border-solid border-2 border-[#e84118] p-[1px] rounded-full"
+          height="44"
+          width="44"
+          src="https://mir-s3-cdn-cf.behance.net/user/50/1148755320017.617fd8ae2afe4.jpg"
+          alt="avatar"
+        />
+        <div class="grid grid-rows-2 gap-1">
+          <span class="font-semibold">Quang Vu Huy</span>
+          <div>
+            <span class="text-zinc-800">Lập trình viên</span>
+          </div>
+        </div>
+      </div>
+      <ul class="flex gap-[2rem] font-semibold tracking-[2px]">
+        <li class="hover:text-[#fdcb6e] transition delay-[45ms] text-navba">
+          <NuxtLink to="/">TRANG CHỦ</NuxtLink>
+        </li>
+        <li class="hover:text-[#fdcb6e] transition delay-[45ms] text-navba">
+          <NuxtLink to="/about">BÀI VIẾT</NuxtLink>
+        </li>
+        <li class="hover:text-[#fdcb6e] transition delay-[45ms] text-navba">
+          <NuxtLink to="/product/123">LIÊN HỆ</NuxtLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script setup></script>
+
+<style scoped src="./styles.css"></style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,23 +1,6 @@
 <template>
   <div>
-    <header class="drop-shadow-xl bg-white text-[14px] text-[black]">
-      <nav class="container mx-auto px-5 h-[76px] flex justify-between items-center">
-        <div class="grid grid-flow-col auto-cols-max gap-2">
-          <img class="border-solid border-2 border-[#e84118] p-[1px] rounded-full" height="44" width="44" src="https://mir-s3-cdn-cf.behance.net/user/50/1148755320017.617fd8ae2afe4.jpg" alt="avatar">
-          <div class="grid grid-rows-2 gap-1">
-            <span class="font-semibold">Quang Vu Huy</span>
-            <div>
-              
-            </div>
-          </div>
-        </div>
-        <ul class="flex gap-[2rem] font-semibold tracking-[2px]">
-          <li class="hover:text-[#fdcb6e] hover:uppercase"><NuxtLink to="/">TRANG CHỦ</NuxtLink></li>
-          <li class="hover:text-[#fdcb6e] hover:uppercase"><NuxtLink to="/about">BÀI VIẾT</NuxtLink></li>
-          <li class="hover:text-[#fdcb6e] hover:uppercase"><NuxtLink to="/product/123">LIÊN HỆ</NuxtLink></li>
-        </ul>
-      </nav>
-    </header>
+    <Header />
 
     <main class="container mx-auto">
       <NuxtPage />
@@ -29,4 +12,6 @@
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import Header from '../components/Header/index.vue'
+</script>
