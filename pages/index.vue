edd5221b536c8d45ae0591509c038455cdf57a89
unknown <vuhuyquang2k@gmail.com>
2024-04-27 16:59:17 +0700

    update

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,12 @@
 <template>
-  <div>
-    Trang chủ
+  <div class="h-screen w-screen m-0 p-0">
+    <h1 class="text-center text-white">Evans</h1>
+    <div class="flex h-full w-full">
+      <div class="w-1/2 p-0">
+        <img class="h-full w-100 object-cover" src="https://wallpaperaccess.com/full/2997.jpg" alt="Ảnh">
+      </div>
+      <div class="w-1/2 bg-black"></div>
+    </div>
   </div>
 </template>
 
