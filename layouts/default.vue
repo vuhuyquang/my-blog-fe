1797acae8705cd67f1fb0ba01181462834d90b4c
unknown <vuhuyquang2k@gmail.com>
2024-05-06 22:59:12 +0700

    refactor layouts

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="space-y-60">
     <Header />
-
+    
     <main class="container w-screen max-w-full content-custom">
       <NuxtPage />
     </main>
-
+    
     <Footer />
   </div>
 </template>
 
 <script lang="ts" setup>
-import Header from '~/layouts/Header/index.vue'
-import Footer from '~/layouts/Footer/index.vue'
+import Header from '~/components/common/Header/index.vue'
+import Footer from '~/components/common/Footer/index.vue'
 </script>
---
+++ pages/index/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="space-y-60">
+    <Banner />
+    <Introduce />
+    <Service />
+    <Slogan />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import Banner from '~/pages/index/components/Banner/index.vue'
+import Introduce from '~/pages/index/components/Introduce/index.vue'
+import Service from '~/pages/index/components/Service/index.vue'
+import Slogan from '~/pages/index/components/Slogan/index.vue'
+</script>
