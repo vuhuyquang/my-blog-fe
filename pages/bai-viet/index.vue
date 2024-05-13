813112f6308e90e9fdb1fdd410179b26adc4eb69
unknown <vuhuyquang2k@gmail.com>
2024-05-13 23:51:10 +0700

    update code

--- pages/bai-viet/index.vue
+++ pages/bai-viet/index.vue
@@ -1,12 +1,24 @@
 <template>
-  <div class="space-y-60">
-    <div class="container mx-auto px-44 pt-14">
-      <div class="flex">
-        <div class="w-8/12 bg-blue-500">
-          1
+  <div class="space-y-6 md:space-y-10 lg:space-y-12">
+    <div class="container mx-auto px-6 lg:px-24 pt-20">
+      <div class="flex flex-col lg:flex-row cursor-pointer">
+        <div class="w-full lg:w-7/12 bg-yellow-100 rounded-l-lg lg:rounded-l-2xl shadow-lg mb-6 lg:mb-0">
+          <img class="h-full w-full rounded-l-lg lg:rounded-l-2xl" src="https://wallpaperaccess.com/full/14736.jpg" alt="image">
         </div>
-        <div class="w-4/12 bg-green-500">
-          2
+        <div class="w-full lg:w-5/12 rounded-r-lg lg:rounded-r-2xl px-6 lg:px-[1.5rem] py-[1.5rem] flex flex-col gap-5 lg:gap-10 shadow-lg">
+          <h1 class="text-2xl lg:text-3xl xl:text-4xl font-bold text-[#000000cc]">Trên tay Reverb, official WebSocket server từ Laravel</h1>
+          <p class="text-base lg:text-lg xl:text-xl text-stone-500">Cách đây không lâu, phiên bản Laravel 11 đã chính thức ra mắt. Đi với đó là Reverb, package sẽ mang đến giao tiếp WebSocket cho các ứng dụng Laravel hoàn toàn bằng PHP (không cần cài đặt thêm extension hoặc sử dụng các stack khác</p>
+          <div class="flex justify-between text-xs lg:text-sm">
+            <div class="flex justify-between gap-2 items-center">
+              <img height="26" width="26" class="rounded-full" src="https://files.fullstack.edu.vn/f8-prod/user_avatars/18810/631175d26916f.png" alt="avatar">
+              <span class="text-[#292929]">Vũ Huy Quang</span>
+            </div>
+            <div class="flex justify-between gap-5 text-[#979797] items-center">
+              <span class="rounded-full bg-[#f2f2f2] px-2 py-1 flex justify-between items-center">Công nghệ</span>
+              <span>10 tháng trước</span>
+              <span>3 phút đọc</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
