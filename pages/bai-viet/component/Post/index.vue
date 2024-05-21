1c91c3333c978eea9444fdefc848637ed72dfdfc
unknown <vuhuyquang2k@gmail.com>
2024-05-22 00:38:55 +0700

    update code

---
+++ pages/bai-viet/component/Post/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container mx-auto lg:px-24 pt-20 space-y-12">
+    <div class="flex flex-col lg:flex-row cursor-pointer mb-6 h-[500px]">
+      <div class="w-full lg:w-7/12 bg-yellow-100 rounded-l-lg lg:rounded-l-2xl shadow-lg mb-6 lg:mb-0 overflow-hidden">
+        <img class="h-full w-full rounded-l-lg lg:rounded-l-2xl zoom-on-hover"
+          src="https://wallpaperaccess.com/full/14736.jpg" alt="image">
+      </div>
+      <div
+        class="w-full lg:w-5/12 rounded-r-lg lg:rounded-r-2xl px-6 lg:px-[1.5rem] py-[1.5rem] flex flex-col gap-5 lg:gap-10 shadow-lg">
+        <h1 class="text-2xl lg:text-3xl xl:text-4xl font-bold text-[#000000cc] hover:text-[#000000] transition delay-[45ms]">Trên tay Reverb, official WebSocket
+          server từ Laravel</h1>
+        <p class="text-sm lg:text-base xl:text-lg text-stone-500">Cách đây không lâu, phiên bản Laravel 11 đã chính thức
+          ra mắt. Đi với đó là Reverb, package sẽ mang đến giao tiếp WebSocket cho các ứng dụng Laravel hoàn toàn bằng
+          PHP (không cần cài đặt thêm extension hoặc sử dụng các stack khác</p>
+        <div class="flex justify-between text-xs lg:text-sm">
+          <div class="flex justify-between gap-2 items-center hidden xl:flex">
+            <img height="26" width="26" class="rounded-full"
+              src="https://files.fullstack.edu.vn/f8-prod/user_avatars/18810/631175d26916f.png" alt="avatar">
+            <span class="text-[#292929] text-[10px] lg:text-sm xl:text-sm">Vũ Huy Quang</span>
+          </div>
+          <div class="flex justify-between gap-5 text-[#979797] items-center text-[10px] lg:text-sm xl:text-sm">
+            <span class="rounded-full bg-[#f2f2f2] px-2 py-1 flex justify-between items-center">Công nghệ</span>
+            <span>10 tháng trước</span>
+            <span>3 phút đọc</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="flex flex-col lg:flex-row gap-10 h-56">
+      <div class="w-full lg:w-1/3 bg-gray-100 rounded-lg shadow-lg cursor-pointer">
+        123
+      </div>
+      <div class="w-full lg:w-1/3 bg-gray-100 rounded-lg shadow-lg cursor-pointer">
+        123
+      </div>
+      <div class="w-full lg:w-1/3 bg-gray-100 rounded-lg shadow-lg cursor-pointer">
+        123
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style scoped src="./styles.css"></style>
